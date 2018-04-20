a0c04e7ab673a5012cd0987746754ab184aded12
Kevin Gong <gyhkevin@outlook.com>
2018-04-20 10:49:39 +0800

    第二次提交p5

--- p5/image-classification/dlnd_image_classification.py
+++ p5/image-classification/dlnd_image_classification.py
@@ -11,7 +11,7 @@
 # 请运行以下单元，以下载 [CIFAR-10 数据集（Python版）](https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz)。
 # 
 
-# In[2]:
+# In[1]:
 
 
 """
@@ -76,7 +76,7 @@ tests.test_folder_path(cifar10_dataset_folder_path)
 # 问问你自己：“可能的标签有哪些？”、“图片数据的值范围是多少？”、“标签是按顺序排列，还是随机排列的？”。思考类似的问题，有助于你预处理数据，并使预测结果更准确。
 # 
 
-# In[3]:
+# In[2]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -98,7 +98,7 @@ helper.display_stats(cifar10_dataset_folder_path, batch_id, sample_id)
 # 在下面的单元中，实现 `normalize` 函数，传入图片数据 `x`，并返回标准化 Numpy 数组。值应该在 0 到 1 的范围内（含 0 和 1）。返回对象应该和 `x` 的形状一样。
 # 
 
-# In[14]:
+# In[3]:
 
 
 def normalize(x):
@@ -128,7 +128,7 @@ tests.test_normalize(normalize)
 # 提示：不要重复发明轮子。
 # 
 
-# In[16]:
+# In[4]:
 
 
 from sklearn import preprocessing
@@ -161,7 +161,7 @@ tests.test_one_hot_encode(one_hot_encode)
 # 运行下方的代码单元，将预处理所有 CIFAR-10 数据，并保存到文件中。下面的代码还使用了 10% 的训练数据，用来验证。
 # 
 
-# In[68]:
+# In[5]:
 
 
 """
@@ -176,7 +176,7 @@ helper.preprocess_and_save_data(cifar10_dataset_folder_path, normalize, one_hot_
 # 这是你的第一个检查点。如果你什么时候决定再回到该记事本，或需要重新启动该记事本，你可以从这里开始。预处理的数据已保存到本地。
 # 
 
-# In[1]:
+# In[6]:
 
 
 """
@@ -221,7 +221,7 @@ valid_features, valid_labels = pickle.load(open('preprocess_validation.p', mode=
 # 
 # 注意：TensorFlow 中的 `None` 表示形状可以是动态大小。
 
-# In[9]:
+# In[7]:
 
 
 import tensorflow as tf
@@ -282,7 +282,7 @@ tests.test_nn_keep_prob_inputs(neural_net_keep_prob_input)
 # **注意**：对于**此层**，**请勿使用** [TensorFlow Layers](https://www.tensorflow.org/api_docs/python/tf/layers) 或 [TensorFlow Layers (contrib)](https://www.tensorflow.org/api_guides/python/contrib.layers)，但是仍然可以使用 TensorFlow 的 [Neural Network](https://www.tensorflow.org/api_docs/python/tf/nn) 包。对于所有**其他层**，你依然可以使用快捷方法。
 # 
 
-# In[10]:
+# In[8]:
 
 
 def conv2d_maxpool(x_tensor, conv_num_outputs, conv_ksize, conv_strides, pool_ksize, pool_strides):
@@ -331,7 +331,7 @@ tests.test_con_pool(conv2d_maxpool)
 # 实现 `flatten` 函数，将 `x_tensor` 的维度从四维张量（4-D tensor）变成二维张量。输出应该是形状（*部分大小（Batch Size）*，*扁平化图片大小（Flattened Image Size）*）。快捷方法：对于此层，你可以使用 [TensorFlow Layers](https://www.tensorflow.org/api_docs/python/tf/layers) 或 [TensorFlow Layers (contrib)](https://www.tensorflow.org/api_guides/python/contrib.layers) 包中的类。如果你想要更大挑战，可以仅使用其他 TensorFlow 程序包。
 # 
 
-# In[60]:
+# In[9]:
 
 
 def flatten(x_tensor):
@@ -358,7 +358,7 @@ tests.test_flatten(flatten)
 # 
 # 实现 `fully_conn` 函数，以向 `x_tensor` 应用全连接层，形状为（*部分大小（Batch Size）*，*num_outputs*）。快捷方法：对于此层，你可以使用 [TensorFlow Layers](https://www.tensorflow.org/api_docs/python/tf/layers) 或 [TensorFlow Layers (contrib)](https://www.tensorflow.org/api_guides/python/contrib.layers) 包中的类。如果你想要更大挑战，可以仅使用其他 TensorFlow 程序包。
 
-# In[59]:
+# In[10]:
 
 
 def fully_conn(x_tensor, num_outputs):
@@ -384,7 +384,7 @@ tests.test_fully_conn(fully_conn)
 # 
 # **注意**：该层级不应应用 Activation、softmax 或交叉熵（cross entropy）。
 
-# In[12]:
+# In[11]:
 
 
 def output(x_tensor, num_outputs):
@@ -419,7 +419,7 @@ tests.test_output(output)
 # * 返回输出
 # * 使用 `keep_prob` 向模型中的一个或多个层应用 [TensorFlow 的 Dropout](https://www.tensorflow.org/api_docs/python/tf/nn/dropout)
 
-# In[63]:
+# In[12]:
 
 
 def conv_net(x, keep_prob):
@@ -520,7 +520,7 @@ tests.test_conv_net(conv_net)
 # 注意：不需要返回任何内容。该函数只是用来优化神经网络。
 # 
 
-# In[64]:
+# In[13]:
 
 
 def train_neural_network(session, optimizer, keep_probability, feature_batch, label_batch):
@@ -549,7 +549,7 @@ tests.test_train_nn(train_neural_network)
 # 实现函数 `print_stats` 以输出损失和验证准确率。使用全局变量 `valid_features` 和 `valid_labels` 计算验证准确率。使用保留率 `1.0` 计算损失和验证准确率（loss and validation accuracy）。
 # 
 
-# In[67]:
+# In[14]:
 
 
 def print_stats(session, feature_batch, label_batch, cost, accuracy):
@@ -583,7 +583,7 @@ def print_stats(session, feature_batch, label_batch, cost, accuracy):
 #  * ...
 # * 设置 `keep_probability` 表示使用丢弃时保留节点的概率
 
-# In[66]:
+# In[15]:
 
 
 # TODO: Tune Parameters
@@ -597,7 +597,7 @@ keep_probability = 0.7
 # 我们先用单个部分，而不是用所有的 CIFAR-10 批次训练神经网络。这样可以节省时间，并对模型进行迭代，以提高准确率。最终验证准确率达到 50% 或以上之后，在下一部分对所有数据运行模型。
 # 
 
-# In[ ]:
+# In[16]:
 
 
 """
@@ -617,84 +617,11 @@ with tf.Session() as sess:
         print_stats(sess, batch_features, batch_labels, cost, accuracy)
 
 
-# In[ ]:
-
-
-# 在aws上跑出来的结果，不知道哪里代码错了，分数很低，有什么方法可以提升么
-'''
-Checking the Training on a Single Batch...
-Epoch  1, CIFAR-10 Batch 1:  Loss:    21.0131 and Validation Accuracy: 0.175400
-Epoch  2, CIFAR-10 Batch 1:  Loss:     2.4504 and Validation Accuracy: 0.104200
-Epoch  3, CIFAR-10 Batch 1:  Loss:     2.2909 and Validation Accuracy: 0.105000
-Epoch  4, CIFAR-10 Batch 1:  Loss:     2.3024 and Validation Accuracy: 0.105000
-Epoch  5, CIFAR-10 Batch 1:  Loss:     2.3024 and Validation Accuracy: 0.105000
-Epoch  6, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.105000
-Epoch  7, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.105000
-Epoch  8, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch  9, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch 10, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch 11, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch 12, CIFAR-10 Batch 1:  Loss:     2.2907 and Validation Accuracy: 0.104400
-Epoch 13, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104600
-Epoch 14, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch 15, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch 16, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch 17, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch 18, CIFAR-10 Batch 1:  Loss:     2.3023 and Validation Accuracy: 0.104800
-Epoch 19, CIFAR-10 Batch 1:  Loss:     2.3024 and Validation Accuracy: 0.104800
-Epoch 20, CIFAR-10 Batch 1:  Loss:     2.3024 and Validation Accuracy: 0.104800
-Epoch 21, CIFAR-10 Batch 1:  Loss:     2.3049 and Validation Accuracy: 0.104600
-Epoch 22, CIFAR-10 Batch 1:  Loss:     2.3715 and Validation Accuracy: 0.104800
-Epoch 23, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.105000
-Epoch 24, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 25, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 26, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 27, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 28, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 29, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 30, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 31, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104600
-Epoch 32, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 33, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 34, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 35, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.105000
-Epoch 36, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104600
-Epoch 37, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 38, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 39, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 40, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 41, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 42, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 43, CIFAR-10 Batch 1:  Loss:     2.3026 and Validation Accuracy: 0.105600
-Epoch 44, CIFAR-10 Batch 1:  Loss:     2.3026 and Validation Accuracy: 0.107400
-Epoch 45, CIFAR-10 Batch 1:  Loss:     2.3027 and Validation Accuracy: 0.108200
-Epoch 46, CIFAR-10 Batch 1:  Loss:     2.3027 and Validation Accuracy: 0.104600
-Epoch 47, CIFAR-10 Batch 1:  Loss:     2.3027 and Validation Accuracy: 0.104800
-Epoch 48, CIFAR-10 Batch 1:  Loss:     2.3026 and Validation Accuracy: 0.104800
-Epoch 49, CIFAR-10 Batch 1:  Loss:     2.3026 and Validation Accuracy: 0.105000
-Epoch 50, CIFAR-10 Batch 1:  Loss:     2.3026 and Validation Accuracy: 0.105000
-Epoch 51, CIFAR-10 Batch 1:  Loss:     2.3026 and Validation Accuracy: 0.104800
-Epoch 52, CIFAR-10 Batch 1:  Loss:     2.3026 and Validation Accuracy: 0.104800
-Epoch 53, CIFAR-10 Batch 1:  Loss:     2.3026 and Validation Accuracy: 0.104800
-Epoch 54, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 55, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 56, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 57, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 58, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 59, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 60, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 61, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 62, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 63, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-Epoch 64, CIFAR-10 Batch 1:  Loss:     2.3025 and Validation Accuracy: 0.104800
-'''
-
-
 # ### 完全训练模型
 # 
 # 现在，单个 CIFAR-10 部分的准确率已经不错了，试试所有五个部分吧。
 
-# In[ ]:
+# In[17]:
 
 
 """
@@ -730,7 +657,7 @@ with tf.Session() as sess:
 # 
 # 利用测试数据集测试你的模型。这将是最终的准确率。你的准确率应该高于 50%。如果没达到，请继续调整模型结构和参数。
 
-# In[ ]:
+# In[18]:
 
 
 """
